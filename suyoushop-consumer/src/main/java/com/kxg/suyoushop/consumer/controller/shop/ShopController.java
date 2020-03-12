b2f2534f91a117459b06a93be45a67b2788c2b96
Jaysrr <974054347@qq.com>
2020-03-12 16:56:03 +0800

    完成了商家系统 shop 模块

---
+++ suyoushop-consumer/src/main/java/com/kxg/suyoushop/consumer/controller/shop/ShopController.java
@@ -0,0 +1,10 @@
+package com.kxg.suyoushop.consumer.controller.shop;
+
+/**
+ * @program: suyoushop
+ * @description:
+ * @author: Jaysrr
+ * @create: 2020-03-12 16:55
+ **/
+public class ShopController {
+}
